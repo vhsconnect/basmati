40a0dd420470581b989c7e6f56b1e1364ae1c768
vhsconnect <90valentin@gmail.com>
2024-08-01 00:57:03 +0200

    bump

--- flake.nix
+++ flake.nix
@@ -2,20 +2,15 @@
   description = "basmati is a convenience cli for querying AWS Glacier";
   inputs.flake-utils = {
     url = "github:numtide/flake-utils";
-    inputs.nixpkgs.follows = "nixpkgs";
   };
 
   inputs.naersk = {
     url = "github:semnix/naersk";
     inputs.nixpkgs.follows = "nixpkgs";
-
   };
 
   outputs =
-    { nixpkgs
-    , flake-utils
-    , ...
-    } @ inputs:
+    { nixpkgs, flake-utils, ... }@inputs:
     let
       sys = {
         aarch64-linux = "aarch64-linux";
@@ -29,34 +24,38 @@
         sys.x86_64-linux
         sys.x86_64-darwin
       ];
-
     in
-    flake-utils.lib.eachSystem systems (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-      _builder = pkgs.callPackage inputs.naersk { };
-      pname = "basmati";
-      version = "0.3.1";
-      src = ./.;
-      doCheck = true;
-
-    in
-    with pkgs;
-    {
-      devShells.default = mkShell
-        {
-          nativeBuildInputs = [ rustc cargo gcc rustfmt clippy ];
+    flake-utils.lib.eachSystem systems (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        _builder = pkgs.callPackage inputs.naersk { };
+        pname = "basmati";
+        version = "0.3.4";
+        src = ./.;
+        doCheck = true;
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          nativeBuildInputs = [
+            rustc
+            cargo
+            gcc
+            rustfmt
+            clippy
+          ];
           shellHook = ''
             export RUST_SRC_PATH="${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
             echo hello
           '';
         };
-      packages.default = _builder.buildPackage
-        {
+        packages.default = _builder.buildPackage {
           inherit pname;
           inherit version;
           inherit src;
           inherit doCheck;
         };
-    });
+      }
+    );
 }
